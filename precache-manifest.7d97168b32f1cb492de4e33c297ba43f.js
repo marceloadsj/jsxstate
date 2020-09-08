821f4b87173aa6e7a8781a54816ff5906432df9c
Marcelo Jr <marceloadsj1@gmail.com>
2020-09-08 15:32:57 +0200

    Updates

---
+++ precache-manifest.7d97168b32f1cb492de4e33c297ba43f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071f34ed612633ba374940b2782f100e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fc531d8ddb03217bfb3",
+    "url": "./static/css/main.6d1117a3.chunk.css"
+  },
+  {
+    "revision": "2ac59c32b406c936b486",
+    "url": "./static/js/2.94a58133.chunk.js"
+  },
+  {
+    "revision": "f6553e79a6678dbefb9269d529896af5",
+    "url": "./static/js/2.94a58133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc531d8ddb03217bfb3",
+    "url": "./static/js/main.c6cf2abb.chunk.js"
+  },
+  {
+    "revision": "e7fd72e30ea532b8216c",
+    "url": "./static/js/runtime-main.5fa3a9f9.js"
+  }
+]);
