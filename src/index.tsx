ac7fcf437abee09dc6ad3ad034dcbda507d9b477
Marcelo Jr <marceloadsj1@gmail.com>
2020-09-13 19:34:48 +0200

    improve: organizing exports in a better way

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,11 @@
 export { default as Interpret } from './components/Interpret'
-
+export { default as Matches } from './components/Matches'
+export { default as Send } from './components/Send'
 export { default as Value } from './components/Value'
 
 export { default as useValue } from './hooks/useValue'
-
 export { default as useContextMachine } from './hooks/useContextMachine'
 
-export { default as Send } from './components/Send'
-
-export { default as Matches } from './components/Matches'
-export * from './components/Matches'
-
 // export { default as Service } from './components/Service'
-// export * from './components/Service'
-
 // export { default as useSend } from './hooks/useSend'
-// export * from './hooks/useSend'
-
 // export { default as useMatches } from './hooks/useMatches'
-// export * from './hooks/useMatches'
