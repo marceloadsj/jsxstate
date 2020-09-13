da48f1d2a851cb80d05f29051f4959fa6664a68e
Marcelo Jr <marceloadsj1@gmail.com>
2020-09-13 20:30:46 +0200

    chore: removing dead code

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,5 @@ export { default as Value } from './components/Value'
 export { default as useValue } from './hooks/useValue'
 export { default as useContextMachine } from './hooks/useContextMachine'
 
-// export { default as Service } from './components/Service'
 // export { default as useSend } from './hooks/useSend'
 // export { default as useMatches } from './hooks/useMatches'
